42ebb6d1e592c32140b5f25c5c5a1cc09e735885
Zachlesk <zharojas0723@gmail.com>
2023-10-04 09:09:58 -0500

    feat: :sparkles: comienzo de lectura de datos backend con react (mariposas)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
-import './App.css';
+import ReadMariposas  from './components/ReadMariposas.js'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+  <>
+  <Router> 
+    <div className='main'>
+      <h2 className='main-header'> Celestialfly </h2>
+      {/* <div>
+        <Route exact path='/create' component={Create}></Route>
+      </div> */}
+      <div>
+        <Route exact path='/mariposas' component={ReadMariposas}></Route>
+      </div>
+      {/* <div>
+        <Route path='/update' component={Update}></Route>
+      </div> */}
     </div>
-  );
+    </Router>
+  </>)
 }
 
-export default App;
+export default App
---
+++ frontend/src/components/ReadMariposas.js
@@ -0,0 +1,74 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Card, Table, Button } from "semantic-ui-react";
+import { Link } from 'react-router-dom';
+
+
+export default function ReadMariposas() {
+    
+    const [APIdata, setAPIdata] = useState([]);
+    
+    useEffect(()=>{
+        axios.get('http:/localhost:8020/mariposas/all')
+        .then((response)=>{
+            console.log(response.data);
+            setAPIdata(response.data);
+        })
+    },[])
+
+    const setData = (data) => {
+        const { nombre_comun, nombre_cientifico, descripcion, habitat, distribucion, jaula, alimentacion } = data;
+        localStorage.setItem('Nombre comun', nombre_comun);
+        localStorage.setItem('Nombre cientifico', nombre_cientifico);
+        localStorage.setItem('Descripcion', descripcion);
+        localStorage.setItem('Habitat', habitat);
+        localStorage.setItem('Distribución', distribucion);
+        localStorage.setItem('Jaula', jaula);
+        localStorage.setItem('Alimentacion', alimentacion);
+
+    }
+
+      const getData = ()=>{
+        axios.get('http:/localhost:8020/mariposas/all')
+        .then((getData) => {
+            setAPIdata(getData.data)
+            console.log(APIdata);
+        })
+    }
+
+    const onDelete = (id)=>{
+        axios.delete(`http:/localhost:8020/mariposas/delete/${id}`)
+        .then(() => {
+            getData()
+        })  
+    }
+
+    return ( 
+        <div>
+        <Card.Group>
+  {APIdata.map((data) => {
+    return (
+      <div key={data.nombre_cientifico}>
+        <Card>
+          {data.nombre_comun}
+          {data.nombre_cientifico}
+          {data.descripcion}
+          {data.habitat}
+          {data.distribucion}
+        </Card>
+        <Link to='/update'>
+          <Table.Cell>
+            <Button onClick={() => setData(data)}>Actualizar</Button>
+          </Table.Cell>
+        </Link>
+        <Table.Cell>
+          <Button onClick={() => onDelete(data.id)}>Eliminar</Button>
+        </Table.Cell>
+      </div>
+    );
+  })}
+</Card.Group>
+
+        </div>
+     );
+}
